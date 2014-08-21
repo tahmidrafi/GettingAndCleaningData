89e83156a41177d098a8d2f2766064b2e79bf388
Tahmid Rafi <rafi.tahmid@gmail.com>
2014-08-22 03:37:26 +0600

    written code for reading feature names get_feature_names()

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,6 @@
-f
+get_feature_names <- function(file, sep = " ", colClasses = c("integer", "character")) {
+    features <- read.table(file = file, sep = sep, colClasses = colClasses)
+    feature_names <- features[,2]
+    
+    feature_names
+}
